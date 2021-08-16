37ec9799b9350f4c634907eb2b3e1055746337e2
Caleb Jackson <caleb.jackson@caleb-jackson-c02fx29xmd6r.gustocorp.com>
2021-08-15 21:04:21 -0400

    first commit

---
+++ build/CMakeFiles/quiz_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quiz_project.dir/main.cpp.o"
+  "CMakeFiles/quiz_project.dir/main.cpp.o.d"
+  "quiz_project"
+  "quiz_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quiz_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/caleb.jackson/Dev/Learning/cpp_project
+# Build directory: /Users/caleb.jackson/Dev/Learning/cpp_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
